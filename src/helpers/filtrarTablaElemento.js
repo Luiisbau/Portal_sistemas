8a1356905810b863c09cca02e399f1de7a4b95a3
jorgeGruver <jpedroza@gruver.mx>
2023-06-06 08:18:23 -0600

    .

--- src/helpers/filtrarTablaElemento.js
+++ src/helpers/filtrarTablaElemento.js
@@ -1,8 +1,7 @@
 export const filtrarNombre = (elementoSeleccionado, estate, opciones, elementoFiltrar) => {
 
   const opcionesFiltrado = opciones.value.filter(elemento => {
-    console.log(elemento.usuario)
     return !elementoSeleccionado.value.includes(elemento.usuario[elementoFiltrar])
   })
   estate.value = opcionesFiltrado;
-  }
+}
